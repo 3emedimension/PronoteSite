6c98858448aba0c4b1367e98c10c22124a7d5c28
3emedimension <3emedimension.25@gmail.com>
2026-03-06 20:00:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1392,9 +1392,9 @@ def add_no_cache_headers(response):
 init_db()
 
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+
 
 
 
